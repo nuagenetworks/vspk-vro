c54dfb4e0e35ac4847833381de24285a16c3d9a9
Philippe Dellaert <philippe@dellaert.org>
2021-09-15 15:36:53 -0700

    Auto generated from API Specifications for 20.10.6

---
+++ o11nplugin-vspk-core/src/main/java/net/nuagenetworks/vro/vspk/model/NSGMigrationProfile.java
@@ -0,0 +1,271 @@
+/*
+  Copyright (c) 2015, Alcatel-Lucent Inc
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+      * Neither the name of the copyright holder nor the names of its contributors
+        may be used to endorse or promote products derived from this software without
+        specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package net.nuagenetworks.vro.vspk.model;
+import net.nuagenetworks.vro.vspk.model.fetchers.PermissionsFetcher;
+
+import net.nuagenetworks.vro.vspk.model.enums.NSGMigrationProfileEntityScope;
+import net.nuagenetworks.bambou.RestException;
+import net.nuagenetworks.bambou.annotation.RestEntity;
+import net.nuagenetworks.vro.model.BaseObject;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties; 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.vmware.o11n.plugin.sdk.annotation.VsoConstructor;
+import com.vmware.o11n.plugin.sdk.annotation.VsoFinder;
+import com.vmware.o11n.plugin.sdk.annotation.VsoMethod;
+import com.vmware.o11n.plugin.sdk.annotation.VsoObject;
+import com.vmware.o11n.plugin.sdk.annotation.VsoProperty;
+import com.vmware.o11n.plugin.sdk.annotation.VsoRelation;
+
+@VsoFinder(name = Constants.NSGMIGRATIONPROFILE, datasource = Constants.DATASOURCE, image = Constants.NSGMIGRATIONPROFILE_IMAGE_FILENAME, idAccessor = Constants.ID_ACCESSOR, relations = {
+        @VsoRelation(inventoryChildren = true, name = Constants.PERMISSIONS_FETCHER, type = Constants.PERMISSIONS_FETCHER)
+})
+@VsoObject(create = false, strict = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@RestEntity(restName = "nsgmigrationprofile", resourceName = "nsgmigrationprofiles")
+public class NSGMigrationProfile extends BaseObject {
+
+    private static final long serialVersionUID = 1L;
+
+    
+    @JsonProperty(value = "creationDate")
+    protected String creationDate;
+    
+    @JsonProperty(value = "description")
+    protected String description;
+    
+    @JsonProperty(value = "destinationProxyFQDN")
+    protected String destinationProxyFQDN;
+    
+    @JsonProperty(value = "entityScope")
+    protected NSGMigrationProfileEntityScope entityScope;
+    
+    @JsonProperty(value = "externalID")
+    protected String externalID;
+    
+    @JsonProperty(value = "lastUpdatedBy")
+    protected String lastUpdatedBy;
+    
+    @JsonProperty(value = "lastUpdatedDate")
+    protected String lastUpdatedDate;
+    
+    @JsonProperty(value = "name")
+    protected String name;
+    
+    @JsonProperty(value = "owner")
+    protected String owner;
+    
+    @JsonIgnore
+    private PermissionsFetcher permissions;
+    
+    @VsoConstructor
+    public NSGMigrationProfile() {
+        permissions = new PermissionsFetcher(this);
+        }
+
+    @VsoProperty(displayName = "Session", readOnly = true)
+    public Session getSession() {
+        return (Session) super.getSession();
+    }@VsoProperty(displayName = "RestName", readOnly = true)
+    public String getRestName() {
+        return super.getRestName();
+    }
+    @VsoProperty(displayName = "Id", readOnly = false)
+    public String getId() {
+        return super.getId();
+    }
+    @VsoProperty(displayName = "ParentId", readOnly = false)
+    public String getParentId() {
+        return super.getParentId();
+    }
+
+    @VsoProperty(displayName = "ParentType", readOnly = false)
+    public String getParentType() {
+        return super.getParentType();
+    }
+
+    @VsoProperty(displayName = "CreationDate", readOnly = false)
+    public String getCreationDate() {
+        return super.getCreationDate();
+    }
+
+    @VsoProperty(displayName = "UpdatedDate", readOnly = false)
+    public String getLastUpdatedDate() {
+        return super.getLastUpdatedDate();
+    }
+
+    @VsoProperty(displayName = "Owner", readOnly = false)
+    public String getOwner() {
+        return super.getOwner();
+    }
+    @JsonIgnore
+    @VsoProperty(displayName = "CreationDate", readOnly = false)   
+    public String getCreationDate() {
+       return creationDate;
+    }
+
+    @JsonIgnore
+    public void setCreationDate(String value) { 
+        this.creationDate = value;
+    }
+    
+    @JsonIgnore
+    @VsoProperty(displayName = "Description", readOnly = false)   
+    public String getDescription() {
+       return description;
+    }
+
+    @JsonIgnore
+    public void setDescription(String value) { 
+        this.description = value;
+    }
+    
+    @JsonIgnore
+    @VsoProperty(displayName = "DestinationProxyFQDN", readOnly = false)   
+    public String getDestinationProxyFQDN() {
+       return destinationProxyFQDN;
+    }
+
+    @JsonIgnore
+    public void setDestinationProxyFQDN(String value) { 
+        this.destinationProxyFQDN = value;
+    }
+    
+    @JsonIgnore
+    @VsoProperty(displayName = "EntityScope", readOnly = false)   
+    public NSGMigrationProfileEntityScope getEntityScope() {
+       return entityScope;
+    }
+
+    @JsonIgnore
+    public void setEntityScope(NSGMigrationProfileEntityScope value) { 
+        this.entityScope = value;
+    }
+    
+    @JsonIgnore
+    @VsoProperty(displayName = "ExternalID", readOnly = false)   
+    public String getExternalID() {
+       return externalID;
+    }
+
+    @JsonIgnore
+    public void setExternalID(String value) { 
+        this.externalID = value;
+    }
+    
+    @JsonIgnore
+    @VsoProperty(displayName = "LastUpdatedBy", readOnly = false)   
+    public String getLastUpdatedBy() {
+       return lastUpdatedBy;
+    }
+
+    @JsonIgnore
+    public void setLastUpdatedBy(String value) { 
+        this.lastUpdatedBy = value;
+    }
+    
+    @JsonIgnore
+    @VsoProperty(displayName = "LastUpdatedDate", readOnly = false)   
+    public String getLastUpdatedDate() {
+       return lastUpdatedDate;
+    }
+
+    @JsonIgnore
+    public void setLastUpdatedDate(String value) { 
+        this.lastUpdatedDate = value;
+    }
+    
+    @JsonIgnore
+    @VsoProperty(displayName = "Name", readOnly = false)   
+    public String getName() {
+       return name;
+    }
+
+    @JsonIgnore
+    public void setName(String value) { 
+        this.name = value;
+    }
+    
+    @JsonIgnore
+    @VsoProperty(displayName = "Owner", readOnly = false)   
+    public String getOwner() {
+       return owner;
+    }
+
+    @JsonIgnore
+    public void setOwner(String value) { 
+        this.owner = value;
+    }
+    
+    @JsonIgnore
+    @VsoProperty(displayName = "Permissions", readOnly = true)   
+    public PermissionsFetcher getPermissions() {
+        return permissions;
+    }
+    @VsoMethod
+    public void fetch(Session session) throws RestException {
+        super.fetch(session);
+    }
+
+    @VsoMethod
+    public void save(Session session, Integer responseChoice) throws RestException {
+        super.save(session, responseChoice);
+        if (!session.getNotificationsEnabled()) {
+           SessionManager.getInstance().notifyElementUpdated(Constants.NSGMIGRATIONPROFILE, getId());
+        }
+    }
+
+    @VsoMethod
+    public void delete(Session session, Integer responseChoiceObj) throws RestException {
+        int responseChoice = (responseChoiceObj != null) ? responseChoiceObj.intValue() : 1;
+        super.delete(session, responseChoice);
+        if (!session.getNotificationsEnabled()) {
+           SessionManager.getInstance().notifyElementDeleted(Constants.NSGMIGRATIONPROFILE, getId());
+        }
+    }
+    @VsoMethod
+    public void assignPermissions(Session session, Permission[] childRestObjs, Boolean commitObj) throws RestException {
+        boolean commit = (commitObj != null) ? commitObj.booleanValue() : true;
+        super.assign(session, java.util.Arrays.asList(childRestObjs), commit);
+        if (!session.getNotificationsEnabled()) { 
+           SessionManager.getInstance().notifyElementUpdated(Constants.NSGMIGRATIONPROFILE, getId());
+        }
+    }
+    
+    @VsoMethod
+    public void createPermission(Session session, Permission childRestObj, Integer responseChoice, Boolean commitObj) throws RestException {
+        boolean commit = (commitObj != null) ? commitObj.booleanValue() : true;
+        super.createChild(session, childRestObj, responseChoice, commit);
+        if (!session.getNotificationsEnabled()) {
+           SessionManager.getInstance().notifyElementInvalidate(Constants.PERMISSIONS_FETCHER, getId());
+        }
+    }public String toString() {
+        return "NSGMigrationProfile [" + "creationDate=" + creationDate + ", description=" + description + ", destinationProxyFQDN=" + destinationProxyFQDN + ", entityScope=" + entityScope + ", externalID=" + externalID + ", lastUpdatedBy=" + lastUpdatedBy + ", lastUpdatedDate=" + lastUpdatedDate + ", name=" + name + ", owner=" + owner + ", id=" + id + ", parentId=" + parentId + ", parentType=" + parentType + ", creationDate=" + creationDate + ", lastUpdatedDate="
+                 + lastUpdatedDate + ", owner=" + owner  + "]";
+    }
+}
